e250ed9a6f810a00c2038cf9676ac879cd8d363f
leoonn <flateia13@hotmail.com>
2021-07-17 19:00:39 -0300

    Initial Commit

---
+++ CamFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public Transform player;
+    public float speedSmooth = 0.3f; 
+    public Vector3 offset; //can be config on inspector 
+
+    
+    
+    void LateUpdate() //LateUpdate is called after all Update functions have been called.
+    {
+        Vector3 CamPosition = player.position + offset; 
+        Vector3 smoothposition = Vector3.Lerp(transform.position, CamPosition, speedSmooth); //Interpolates between the points and by the interpolant .
+        transform.position = smoothposition;
+    }
+}
